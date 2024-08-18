e68e347446f2511e25acbc85649b2250eacbceb5
Eugene Gershnik <gershnik@users.noreply.github.com>
2024-08-17 22:48:29 -0700

    Proper module exports

--- src/wakeonlan/__init__.py
+++ src/wakeonlan/__init__.py
@@ -6,3 +6,11 @@
 """wakeonlan package"""
 
 from .wakeonlan import VERSION as __version__, wake, save_name, get_name_record, get_names, delete_name
+
+__all__ = [
+    'wake',
+    'save_name',
+    'get_name_record',
+    'get_names',
+    'delete_name'
+]
