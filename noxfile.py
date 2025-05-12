93d8da8be68cb79d44ce04a06466848aaa9c0323
Eugene Gershnik <gershnik@users.noreply.github.com>
2025-05-12 11:10:11 -0700

    Updating test matrix

--- noxfile.py
+++ noxfile.py
@@ -1,3 +1,5 @@
+# pylint: disable=missing-module-docstring, missing-function-docstring
+
 import nox
 import re
 from pathlib import Path
@@ -6,13 +8,13 @@ mydir = Path(__file__).parent
 
 extraPythons = []
 if (mydir/".extrapythons").exists():
-    with open(mydir/".extrapythons", "r") as extraPythonsFile:
+    with open(mydir/".extrapythons", "r", encoding="utf-8") as extraPythonsFile:
         for line in extraPythonsFile:
             line = line.strip()
             if len(line) != 0 and not re.match(r'\s*#.*', line):
                 extraPythons.append(line.strip())
 
-@nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11", "3.12"] + extraPythons)
+@nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11", "3.12", "3.13", "3.13t"] + extraPythons)
 def test(session):
     session.install("pytest")
     #session.install("--no-build-isolation", "--editable", ".")
